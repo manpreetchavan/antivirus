1ad45280b5bbb224f0b44f553de5909691f63737
rushike <rushike.ab1@gmail.com>
2019-02-19 22:58:58 +0530

    virus database added

--- start.py
+++ start.py
@@ -1,13 +1,29 @@
-import hashlib as hash
+import hashlib as ha
+import urllib.request as webopener
 import os
-
+hash_ = ha.md5()
 def get_files(dir):
     return [f for f in os.listdir('.') if os.path.isfile(f)]
     
 def string(file_path):
     f = open(file_path, 'r')
     print("file is : ",f.read())
+
+def prepare_data():
+    f = open("sig.data", "r")
+    lines = [line.rstrip('\n') for line in f]
+    lines.sort()
+    fr = open("virus_sig.csv", "w+")
+    for l in lines:
+        fr.write(str(l) + "\n")
 if __name__ == "__main__":
     li = get_files("~/rushikesh/antivirus")
     for f in li:
-        string(f)
+        print("F : ", f)
+        with open(f, "rb") as fi:
+            for chnk in iter(lambda: fi.read(4096), b""):
+                hash_.update(chnk)
+        print(hash_.hexdigest())
+    prepare_data()
+
+
